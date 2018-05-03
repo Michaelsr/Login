dfcb1d72deca171383bcc9133af9a2b2682508f9
Michaelsr <michaelsonccoramos@gmail.com>
2018-05-03 13:55:30 -0500

    Micc -V3.0

--- app/src/main/java/com/upeu/msr/examen/MenuUser.java
+++ app/src/main/java/com/upeu/msr/examen/MenuUser.java
@@ -1,5 +1,6 @@
 package com.upeu.msr.examen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +84,10 @@ public class MenuUser extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+
+            Intent escaner = new Intent(MenuUser.this, Escaner.class);
+            startActivity(escaner);
+
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
