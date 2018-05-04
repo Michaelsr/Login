1df21dca11a72dfc2a99e6e5589f3f7add078d1c
Michaelsr <michaelsonccoramos@gmail.com>
2018-05-04 14:33:09 -0500

    Micc -V3.1

--- app/src/main/java/com/upeu/msr/examen/Escaner.java
+++ app/src/main/java/com/upeu/msr/examen/Escaner.java
@@ -2,12 +2,41 @@ package com.upeu.msr.examen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class Escaner extends AppCompatActivity {
+    EditText codigo;
+    Button escaner;
+    private ZXingScannerView vistaescaner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escaner);
     }
+    public void Escaner(View view){
+        vistaescaner = new ZXingScannerView(this);
+        vistaescaner.setResultHandler(new zxingscanner());
+        setContentView(vistaescaner);
+        vistaescaner.startCamera();
+    }
+
+    class zxingscanner implements ZXingScannerView.ResultHandler{
+
+        @Override
+        public void handleResult(Result result) {
+            String dato = result.getText();
+            setContentView(R.layout.activity_escaner);
+            vistaescaner.stopCamera();
+            codigo = (EditText)findViewById(R.id.ecaner_text);
+            codigo.setText(dato);
+        }
+    }
+
 }
