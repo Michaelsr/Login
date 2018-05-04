c4d4090e8ef2f994dd3a145201f776c35ca3743a
Michaelsr <michaelsonccoramos@gmail.com>
2018-05-04 14:58:53 -0500

    Micc -V3.1.0

--- app/src/main/java/com/upeu/msr/examen/Escaner.java
+++ app/src/main/java/com/upeu/msr/examen/Escaner.java
@@ -3,7 +3,6 @@ package com.upeu.msr.examen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.zxing.Result;
@@ -12,7 +11,6 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class Escaner extends AppCompatActivity {
     EditText codigo;
-    Button escaner;
     private ZXingScannerView vistaescaner;
 
     @Override
